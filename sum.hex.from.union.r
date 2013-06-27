7c3bfda026aaf9e434e2e08aefb7a7d75849510a
Chad Wilsey <chadwilsey@gmail.com>
2013-06-27 08:48:41 -0700

    update sum.hex.from.union.r

--- sum.hex.from.union.r
+++ sum.hex.from.union.r
@@ -3,6 +3,7 @@ setwd('c:/users/cbwilsey/documents/postdoc/scripts/')
 sum.hex <- function(x,attr.table,max.area=866025.4)
 
 {
+	if (x%%10000==0) { cat(x,(Sys.time()-startTime),'\n') }
 	# water <- attr.table[attr.table$hex_id==x & attr.table$fid_water!=(-1),]
 	# print(water)
 	# water <- as.vector(water[,'area'])
@@ -22,19 +23,21 @@ sum.hex <- function(x,attr.table,max.area=866025.4)
 # Read in the data
 # ====================================================================================
 
-# temp <- read.csv("E:/SpatialData/National Hydrography/union1.txt")
-# print(head(temp))
-# temp <- temp[temp$Hex_ID!=0 & temp$FID_albers_test3_hexagons_1km!=(-1),c('Hex_ID','FID_pnw_all_water','Shape_Area')]
-# colnames(temp) <- c('hex_id','fid_water','area')
+theData <- read.csv("E:/SpatialData/National Hydrography/union1.txt")
+print(head(theData))
+theData <- theData[theData$Hex_ID!=0 & theData$FID_albers_test3_hexagons_1km!=(-1),c('Hex_ID','FID_pnw_all_water','Shape_Area')]
+colnames(theData) <- c('hex_id','fid_water','area')
 
 # ===================================================================================
 # Run the analysis
 # ==================================================================================
 
-the.areas <- sapply(X=seq(1,6495260,1),FUN=sum.hex,attr.table=temp)
-# the.areas <- sapply(X=seq(8,12,1),FUN=sum.hex,attr.table=temp)
+startTime <- Sys.time()
+# the.areas <- sapply(X=seq(1,6495260,1),FUN=sum.hex,attr.table=theData)
+the.areas <- sapply(X=seq(1,10000,1),FUN=sum.hex,attr.table=theData)
+# the.areas <- sapply(X=seq(8,12,1),FUN=sum.hex,attr.table=theData)
 # print(the.areas)
 
 output <- data.frame(hex_id=seq(1,6495260,1),proportion=the.areas)
-write.csv(output,"E:/SpatialData/National Hydrography/hexagon_proporation1.csv")
+write.csv(output,"E:/SpatialData/National Hydrography/hexagon.proportion.v1.csv")
 
