cd33fc43efe69771763e697273b56fa968ca2323
Chad Wilsey <cbwilsey@uw.edu>
2013-07-09 14:25:17 -0700

    update, density

--- density.r
+++ density.r
@@ -20,12 +20,14 @@ density.est <- function(census,trait.col=list(),areas,time.step)
 ha.hex <- 86.6 # ha, divide by 100 to get km2
 
 # n.s <- read.csv('F:/PNWCCVA_Data2/HexSim/Workspaces/lynx_v1/Analysis/north.south.50.csv',header=TRUE)
-# sw.range3 <- read.csv('F:/PNWCCVA_Data2/HexSim/Workspaces/lynx_v1/Analysis/sw.range3.csv',header=TRUE)
 # hab.qual <- read.csv('F:/PNWCCVA_Data2/HexSim/Workspaces/lynx_v1/Analysis/hab.qual.15.csv',header=TRUE)
+sw.range <- read.csv('F:/PNWCCVA_Data2/HexSim/Workspaces/lynx_v1/Analysis/sw.range.csv',header=TRUE)
+# sw.range <- read.csv('F:/PNWCCVA_Data2/HexSim/Workspaces/lynx_v1/Analysis/sw.range2.csv',header=TRUE)
+# sw.range <- read.csv('F:/PNWCCVA_Data2/HexSim/Workspaces/lynx_v1/Analysis/sw.range3.csv',header=TRUE)
 
-area.1 <- length(n.s$Step_1[n.s$Step_1==1 & sw.range3$Step_1==1 & hab.qual$Score >= 0.25]) * ha.hex / 100
-area.2 <- length(n.s$Step_1[n.s$Step_1==2 & sw.range3$Step_1==1 & hab.qual$Score >= 0.25]) * ha.hex / 100
-print(area.1); print(area.2)
+area.1 <- length(n.s$Step_1[n.s$Step_1==1 & sw.range$Step_1==1 & hab.qual$Score >= 0.25]) * ha.hex / 100
+area.2 <- length(n.s$Step_1[n.s$Step_1==2 & sw.range$Step_1==1 & hab.qual$Score >= 0.25]) * ha.hex / 100
+cat('area 1',area.1,'area 2',area.2,'\n')
 
 # calculating densities from census files
 # lynx.dens <- density.est(census='F:/PNWCCVA_Data2/HexSim/Workspaces/lynx_v1/Results/lynx.007/lynx.007-[1]/lynx.007.0.csv',
@@ -35,10 +37,10 @@ print(area.1); print(area.2)
 							# )
 # print(lynx.dens)
 
-scenario <- 'lynx.027'
-cat(scenario,'\n')
+scenario <- 'lynx.032'
+cat('###################\n',scenario,'\n')
 plot(1~1,ylim=c(1,4000),xlim=c(1,50))
-for (y in 1:40)
+for (y in 1:20)
 {
 	lynx.dens <- density.est(census=paste('F:/PNWCCVA_Data2/HexSim/Workspaces/lynx_v1/Results/',scenario,'/',scenario,'-[1]/',scenario,'.0.csv',sep=''),
 								trait.col=list(seq(7,12,1),seq(13,18,1)),
