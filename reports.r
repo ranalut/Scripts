de3b2501f735cf2e29c778707ebbeb188cfeaa58
Chad Wilsey <cbwilsey@uw.edu>
2013-10-09 14:37:44 -0700

    update for lynx.r

--- reports.r
+++ reports.r
@@ -27,19 +27,20 @@
 # For H drive on tower...
 report4 <- function(hexsim.wksp1, hexsim.wksp2, spp.folder, scenario.name, report.type, start.year, stop.year, population, trait,rep.no)
 {
+	startTime <- Sys.time()
 	command <- paste(substr(hexsim.wksp1,1,2),' && cd "',hexsim.wksp1,'\\currentHexSim" && OutputTransformer.exe -',report.type,':',start.year,':',stop.year,':"',population,'":"',trait,'" "',hexsim.wksp2,'\\Workspaces\\',spp.folder,'\\Results\\',scenario.name,'\\',scenario.name,'-[',rep.no,']\\',scenario.name,'.log"',sep='')
 	# print(command); stop('cbw')
 	shell(command)
+	print(Sys.time()-startTime)
 }
 
 
-base.sim <- 'gulo.023.a2.'
-base.sim <- 'gulo.023.'
-# gcms <- c('ccsm3','cgcm3','giss-er','hadcm3','miroc')
-gcms <- 'miroc'
-gcms <- 'baseline'
-other <- c('','.biomes','.swe')
-other <- ''
+# base.sim <- 'gulo.023.a2.' # base.sim <- 'gulo.023.'
+base.sim <- 'lynx.050.'
+gcms <- c('baseline') # c('ccsm3','cgcm3','giss-er','hadcm3','miroc') # c('baseline')
+# gcms <- 'ccsm3'
+other <- c('','.35') # other <- '' # other <- c('','.biomes','.swe')
+
 scenarios <- NA
 for (i in gcms)
 {
@@ -51,66 +52,72 @@ for (i in gcms)
 scenarios <- scenarios[-1]
 print(scenarios)
 # stop('cbw')
-years <- list(c(11,20),c(21,30),c(31,40),c(41,50),c(51,60),c(61,70),c(71,80),c(81,90),c(91,100),c(101,109))
-years <- list(c(11,20),c(21,30),c(31,40),c(41,50))
-
+# years <- list(c(11,20),c(21,30),c(31,40),c(41,50),c(51,60),c(61,70),c(71,80),c(81,90),c(91,100),c(101,109)) # years <- list(c(11,20),c(21,30),c(31,40),c(41,50)) # Wolverine
+# years <- list(c(16,24),c(25,33),c(34,42),c(43,51),c(52,60),c(61,69),c(70,78),c(79,87),c(88,96),c(97,105)) # Lynx
+# years <- list(c(25,33),c(34,42),c(43,51),c(79,87),c(88,96),c(97,105)) # Only mid-century and end-of-century. # Lynx
+years <- list(c(16,42)) # years <- list(c(25,51),c(79,105))
 # stop('cbw')
 
 for (i in scenarios)
 {
 	for (j in 1:5)
 	{
-    for (k in 1:length(years))
-    {
-      # print(paste('H:/HexSim/Workspaces/wolverine_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_wolverine_',years[[k]][1],'_',years[[k]][2],'_[ProAreas].csv',sep='')); stop('cbw')
-	  # if (file.exists(paste('H:/HexSim/Workspaces/wolverine_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_wolverine_',years[[k]][1],'_',years[[k]][2],'_[ProAreas].csv',sep=''))==FALSE) 
-		# {
-			# print(paste('H:/HexSim/Workspaces/wolverine_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_wolverine_',years[[k]][1],'_',years[[k]][2],'_[ProAreas].csv',sep='')); stop('cbw')
-		# }
-	  if (file.exists(paste('H:/HexSim/Workspaces/wolverine_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_wolverine_',years[[k]][1],'_',years[[k]][2],'_[ProAreas].csv',sep=''))==TRUE)
+		for (k in 1:length(years))
 		{
-			print(paste('H:/HexSim/Workspaces/wolverine_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_wolverine_',years[[k]][1],'_',years[[k]][2],'_[ProAreas].csv',sep='')); next(k)
+			if (file.exists(paste('I:/HexSim/Workspaces/lynx_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_lynx_',years[[k]][1],'_',years[[k]][2],'_[HucID].csv',sep=''))==FALSE)
+			{
+				report4(
+					hexsim.wksp1= 'F:\\PNWCCVA_Data2\\HexSim', # 'D:/Data/wilsey/',
+					hexsim.wksp2='I:\\HexSim', # 'H:\\HexSim', # 'D:\\Data\\wilsey',
+					spp.folder='lynx_v1', # 'wolverine_v1',
+					scenario.name=i,
+					report.type='productivity',
+					start.year=years[[k]][1],
+					stop.year=years[[k]][2],
+					population='lynx', # 'wolverine',
+					trait='HucID',
+					rep.no=j
+					)
+				# stop('cbw')
+			}
+			else { print(paste('I:/HexSim/Workspaces/lynx_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_lynx_',years[[k]][1],'_',years[[k]][2],'_[HucID].csv',sep='')) }
+			
+			if (file.exists(paste('I:/HexSim/Workspaces/lynx_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_lynx_',years[[k]][1],'_',years[[k]][2],'_[EcoRegion].csv',sep=''))==FALSE)
+			{
+				report4(
+					hexsim.wksp1= 'F:\\PNWCCVA_Data2\\HexSim', # 'D:/Data/wilsey/',
+					hexsim.wksp2='I:\\HexSim', # ''H:\\HexSim', # 'D:\\Data\\wilsey',
+					spp.folder='lynx_v1', # 'wolverine_v1',
+					scenario.name=i,
+					report.type='productivity',
+					start.year=years[[k]][1],
+					stop.year=years[[k]][2],
+					population='lynx', # 'wolverine',
+					trait='EcoRegion',
+					rep.no=j
+					)
+			}
+			else { print(paste('I:/HexSim/Workspaces/lynx_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_lynx_',years[[k]][1],'_',years[[k]][2],'_[EcoRegion].csv',sep='')) }
+			
+			if (file.exists(paste('I:/HexSim/Workspaces/lynx_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_lynx_',years[[k]][1],'_',years[[k]][2],'_[ProAreas].csv',sep=''))==FALSE)
+			{
+				report4(
+					hexsim.wksp1= 'F:\\PNWCCVA_Data2\\HexSim', # 'D:/Data/wilsey/',
+					hexsim.wksp2='I:\\HexSim', # ''H:\\HexSim', # 'D:\\Data\\wilsey',
+					spp.folder='lynx_v1', # 'wolverine_v1',
+					scenario.name=i,
+					report.type='productivity',
+					start.year=years[[k]][1],
+					stop.year=years[[k]][2],
+					population='lynx', # 'wolverine',
+					trait='ProAreas',
+					rep.no=j
+					)
+			}
+			else { print(paste('I:/HexSim/Workspaces/lynx_v1/Results/',i,'/',i,'-[',j,']/',i,'_REPORT_productivity_lynx_',years[[k]][1],'_',years[[k]][2],'_[ProAreas].csv',sep='')) }
+			# stop('cbw')
+			cat(i,' rep ',j,years[[k]],'\n')
 		}
-	  report4(
-    		hexsim.wksp1= 'F:\\PNWCCVA_Data2\\HexSim', # 'D:/Data/wilsey/',
-    		hexsim.wksp2='H:\\HexSim', # 'D:\\Data\\wilsey',
-    		spp.folder='wolverine_v1',
-    		scenario.name=i,
-    		report.type='productivity',
-    		start.year=years[[k]][1],
-    		stop.year=years[[k]][2],
-    		population='wolverine',
-    		trait='HucID',
-    		rep.no=j
-    		)
-   	 # stop('cbw')
-     report4(
-    		hexsim.wksp1= 'F:\\PNWCCVA_Data2\\HexSim', # 'D:/Data/wilsey/',
-    		hexsim.wksp2='H:\\HexSim', # 'D:\\Data\\wilsey',
-    		spp.folder='wolverine_v1',
-    		scenario.name=i,
-    		report.type='productivity',
-    		start.year=years[[k]][1],
-    		stop.year=years[[k]][2],
-    		population='wolverine',
-    		trait='EcoRegion',
-    		rep.no=j
-    		)
-      report4(
-    		hexsim.wksp1= 'F:\\PNWCCVA_Data2\\HexSim', # 'D:/Data/wilsey/',
-    		hexsim.wksp2='H:\\HexSim', # 'D:\\Data\\wilsey',
-    		spp.folder='wolverine_v1',
-    		scenario.name=i,
-    		report.type='productivity',
-    		start.year=years[[k]][1],
-    		stop.year=years[[k]][2],
-    		population='wolverine',
-    		trait='ProAreas',
-    		rep.no=j
-    		)
-    	# stop('cbw')
-      cat(i,' rep ',j,'\n')
-   	}
 	}
 }
 stop('cbw')
