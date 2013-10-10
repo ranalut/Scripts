f2db1035b0f59dbc8c537a6036415c9ce8be2fbb
Chad Wilsey <cbwilsey@uw.edu>
2013-10-10 11:38:10 -0700

    table.census.r, separated this from other code

---
+++ table.census.r
@@ -0,0 +1,117 @@
+
+library(RColorBrewer)
+source('scenario.vector.r')
+
+table.census.traits <- function(workspace, scenario, merge.table, reps, census.no, trait.name, years)
+{
+	test <- file.exists(paste(workspace,scenario,'/',scenario,'-[',reps,']/',scenario,'.',census.no,'.csv',sep=''))
+	# cat(test,'\n')
+	if (test==FALSE) { print('Cannot find file.'); return(NA) }
+	census <- read.csv(paste(workspace,scenario,'/',scenario,'-[',reps,']/',scenario,'.',census.no,'.csv',sep=''),header=TRUE)
+	# print(census[(years[1]+1),]); stop('cbw')
+	census2 <- census[,7:dim(census)[2]]
+
+	census2 <- census2[,(merge.table$trait.index + 1)]
+	colnames(census2) <- paste(trait.name,merge.table$shape.index,sep='')
+	
+	census3 <- data.frame(census[,3:5],census2)
+	census3 <- census3[(years[1]+1):(years[2]+1),]
+	# print(census3); stop('cbw')
+	
+	the.means <- as.data.frame(round(apply(census3,MARGIN=2,FUN=mean)))
+	colnames(the.means) <- 'the.mean'
+	# print(the.means); stop('cbw')
+	
+	# census3 <- data.frame(census[,1:6],census2)
+	# print(head(census3))
+	write.csv(the.means, paste(workspace,scenario,'/',scenario,'-[',reps,']/',scenario,'.',trait.name,'.',years[1],'.',years[2],'.csv',sep=''))
+	# return(census3)
+}
+
+the.change <- function(workspace, baseline, fut.scenarios, reps, trait.name, base.years, years, type)
+{
+	# Baseline Mean
+	scenario <- baseline
+	output <- read.csv(paste(workspace ,scenario,'/',scenario,'-[1]/',scenario,'.',trait.name,'.',base.years[1],'.',base.years[2],'.csv',sep=''),header=TRUE, row.names=1)
+	# print(head(output)); stop('cbw')
+	for (i in 2:reps)
+	{
+		temp <- read.csv(paste(workspace ,scenario,'/',scenario,'-[',i,']/',scenario,'.',trait.name,'.',base.years[1],'.',base.years[2],'.csv',sep=''),header=TRUE, row.names=1)
+		output <- cbind(output,temp)
+	}
+	# print(head(output)); stop('cbw')
+	temp.sum <- as.numeric(apply(output,MARGIN=1,mean))
+	output$base.mean <- temp.sum
+	write.csv(output,paste(workspace ,scenario,'/mean.',scenario,'.',trait.name,'.',base.years[1],'.',base.years[2],'.csv',sep=''))
+	base.mean <- output$base.mean
+	
+	# Future Scenarios
+	for (j in fut.scenarios)
+	{
+		scenario <- j
+		output <- read.csv(paste(workspace ,scenario,'/',scenario,'-[1]/',scenario,'.',trait.name,'.',years[1],'.',years[2],'.csv',sep=''),header=TRUE, row.names=1)
+		for (i in 2:reps)
+		{
+			temp <- read.csv(paste(workspace ,scenario,'/',scenario,'-[',i,']/',scenario,'.',trait.name,'.',years[1],'.',years[2],'.csv',sep=''),header=TRUE, row.names=1)
+			output <- cbind(output,temp)
+		}
+		# print(head(output)); stop('cbw')
+		temp.sum <- as.numeric(apply(output,MARGIN=1,mean))
+		output$rep.mean <- temp.sum
+		
+		if (type=='p') { output$p.change <- round(100 * ((output$rep.mean - base.mean) / base.mean)) }
+		if (type=='abs') { output$abs.change <- round(output$rep.mean - base.mean) }
+		print(head(output))
+		write.csv(output,paste(workspace,scenario,'/',type,'.change.',scenario, '.',trait.name,'.',years[1],'.',years[2],'.csv',sep=''))
+	}
+	# return(output)
+}	
+
+huc.table <- data.frame(shape.index=seq(1,1549,1),trait.index=seq(1,1549,1))
+pro.area.table <- data.frame(shape.index=seq(1,1252,1),trait.index=seq(1,1252,1))
+ecoregion.table <- read.csv('H:/SpatialData/SpatialData/tnc-terr-ecoregions121409/ecoregions.merge.table.csv',header=TRUE,row.names=1)
+colnames(ecoregion.table) <- c('shape.index','ECO_NAME','trait.index')
+
+folder <- 'lynx_v1' # 'wolverine_v1'
+scenarios <- scenarios.vector(
+				base.sim='lynx.050.', # base.sim='gulo.023.a2.',# base.sim='gulo.023.',
+				gcms=c('baseline','ccsm3','cgcm3','giss-er','hadcm3','miroc'),# gcms=c('ccsm3','cgcm3','giss-er','hadcm3','miroc'), gcms='baseline',
+				other=c('','.35') # other=c('','.biomes','.swe')# other=''
+				)
+for (i in scenarios)
+{
+	for (j in 1:5)
+	{
+		table.census.traits(
+			workspace=paste('I:/HexSim/Workspaces/',folder,'/Results/',sep=''), # workspace=paste('H:/HexSim/Workspaces/',folder,'/Results/',sep=''), 
+			scenario=i, 
+			merge.table=huc.table,
+			reps=j,
+			census.no=2,
+			trait.name='huc',
+			years=c(25,51)
+			)
+		cat(i,j,'\n')
+		# stop('cbw')
+	}
+}
+
+# the.change(workspace=paste('H:/HexSim/Workspaces/',folder,'/Results/',sep=''), baseline='gulo.023.baseline', fut.scenarios=scenarios, reps=5, trait.name='huc', base.years=c(41,50), years=c(41,50), type='abs')
+the.change(workspace=paste('I:/HexSim/Workspaces/',folder,'/Results/',sep=''), baseline='lynx.050.baseline', fut.scenarios=scenarios, reps=5, trait.name='huc', base.years=c(25,51), years=c(41,50), type='abs')
+
+stop('cbw')
+
+# for (i in 1:dim(ecoregion.table)[1])
+# # for (i in 1)
+# {
+	# plot.census.traits(
+		# workspace=paste('F:/PNWCCVA_Data2/HexSim/Workspaces/',folder,'/',sep=''), 
+		# max.t=max.t, 
+		# scenarios=scenarios, 
+		# eco.region=i,
+		# y.max=y.max[i], 
+		# # main.text=paste(ecoregion.table$ECO_NAME[i],'\nboreal cycling',sep='')
+		# main.text=ecoregion.table$ECO_NAME[i]
+		# )
+# }
+
