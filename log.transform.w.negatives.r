4960d492c8fe34b7ac51e218cb95f838e884c807
Chad Wilsey <cbwilsey@uw.edu>
2013-10-11 16:30:44 -0700

    log.transform.w.negatives.r update

--- log.transform.w.negatives.r
+++ log.transform.w.negatives.r
@@ -2,8 +2,14 @@
 
 log.transform.neg <- function(x)
 {
+	if (is.na(x)==TRUE) { return(NA) }
 	x <- round(x)
-	if(x>0) { temp <- log(x); return(temp) }
+	if(x>1) { temp <- log(x); return(temp) }
 	if(x==0) { return(0) }
-	if(x<0) { temp <- -1*log(abs(x)); return(temp) }
+	if(x<(-1)) { temp <- -1*log(abs(x)); return(temp) }
+	if(x<=1 & x>0) { temp <- 0.01; return(temp) }
+	if(x>=(-1) & x<0) { temp <- -0.01; return(temp) }
 }
+
+
+# print(sapply(c(NA,-5,-1,-0.5,0,0.5,1,5),log.transform.neg))
