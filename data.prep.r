d98187e0e7944024db93640bbd42d12f5fd065b3
Chad Wilsey <cbwilsey@uw.edu>
2013-10-11 10:04:15 -0700

    data.prep.r saved as own script
    for creating figures, prepares data.

---
+++ data.prep.r
@@ -0,0 +1,26 @@
+
+data.prep <- function(data.file, type, var.name=NA)
+{
+	
+	if (type=='productivity')
+	{
+		the.data <- read.csv(data.file,header=TRUE, stringsAsFactors=FALSE, row.names=1)
+		the.data <- the.data[,c(1,dim(the.data)[2])]
+		# print(head(the.data)); stop('cbw')
+		colnames(the.data) <- c('CBW_CODE','variable')
+		return(the.data)
+	}
+	if (type=='abundance')
+	{
+		the.data <- read.csv(data.file, stringsAsFactors=FALSE, row.names=1, skip=4, header=FALSE)
+		last.col <- dim(the.data)[2]
+		# print(head(the.data))
+		the.data$CBW_CODE <- sapply(rownames(the.data),FUN=extract.number,simplify=TRUE,var.name=var.name)
+		# print(head(the.data))
+		the.data <- the.data[,c(last.col,(last.col+1))]
+		colnames(the.data) <- c('variable','CBW_CODE')
+		# print(head(the.data))
+		# print(table(the.data$variable))
+		return(the.data)
+	}
+}
