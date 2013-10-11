f582ab43cff1bbe44d6ee841d4c82af2a246db20
Chad Wilsey <cbwilsey@uw.edu>
2013-10-11 11:39:26 -0700

    plot.polygons.r set up for density b-d

--- plot.polygons.r
+++ plot.polygons.r
@@ -4,87 +4,40 @@ library(sp)
 library(RColorBrewer)
 library(fields)
 
+source('data.prep.r')
+source('map.plot.r')
+
 extract.number <- function(x,var.name) { temp <- as.numeric(strsplit(x,split=var.name)[[1]][2]); return(temp) }
 
-data.prep <- function(data.file, type, var.name)
+create.figure <- function(workspace, species.folder, baseline.scenario, future.scenario, spatial.variable, baseline.time, time.window, historical.cutoffs, future.cutoffs, data.type)
 {
-	
-	if (type=='productivity')
-	{
-		the.data <- read.csv(data.file,header=TRUE, stringsAsFactors=FALSE)
-		the.data <- the.data[,c(1,4)]
-		colnames(the.data) <- c('CBW_CODE','variable')
-		return(the.data)
-	}
-	if (type=='abundance')
-	{
-		the.data <- read.csv(data.file, stringsAsFactors=FALSE, row.names=1, skip=4, header=FALSE)
-		last.col <- dim(the.data)[2]
-		# print(head(the.data))
-		the.data$CBW_CODE <- sapply(rownames(the.data),FUN=extract.number,simplify=TRUE,var.name=var.name)
-		# print(head(the.data))
-		the.data <- the.data[,c(last.col,(last.col+1))]
-		colnames(the.data) <- c('variable','CBW_CODE')
-		# print(head(the.data))
-		# print(table(the.data$variable))
-		return(the.data)
-	}
-}
-
-# Function to produce a plot, must be wrapped in png() and layout() functions
-map.plot <- function(baseline.data, distribution.data, cutoffs, spatial.data, political, ocean, model.name, add.legend, include.axes, color.ramp, margins, label.text=NA)
-{
-	the.range <- range(baseline.data$variable); print(the.range)
-	baseline.data$variable[baseline.data$variable < cutoffs[1]] <- cutoffs[1] + 1
-	baseline.data$variable[baseline.data$variable > cutoffs[length(cutoffs)]] <- cutoffs[length(cutoffs)] - 1
-	baseline.data$variable[baseline.data$variable==0] <- NA
-	
-	baseline.map <- merge(spatial.data,baseline.data,all.x=FALSE)
-	
-	colnames(distribution.data)[1] <- 'hist.range'
-	baseline.map <- merge(baseline.map,distribution.data,all.x=TRUE)
-	# print(head(baseline.map@data))
-	
-	baseline.map@data$color <- as.character(cut(baseline.map@data$variable, breaks=cutoffs, labels=color.ramp))
-	# baseline.map@data$color <- as.character(colorRampPalette(color.ramp)(baseline.map@data$variable))
-	# print(head(baseline.map@data$color)); stop('cbw')
-	
-	par(mar=margins)
-	plot(political, col=rgb(189,189,189,max=255), xlim=c(-135,-107), ylim=c(38,58), axes=include.axes, cex.axis=1.25)
-	plot(baseline.map, add=TRUE, col=baseline.map@data$color, border='gray')
-	plot(ocean, add=TRUE, col=rgb(166,189,219,max=255))
-	if (the.range[1]<0) { plot(baseline.map[is.na(baseline.map@data$variable)==TRUE & baseline.map@data$hist.range>0,],border='white',add=TRUE) }
-	plot(political, add=TRUE)
-	text(x=-130,y=40,model.name,cex=1.5)
-	
-	if (add.legend==TRUE) 
-	{ 
-		# color.legend(xl,yb,xr,yt)
-		image.plot(legend.only=TRUE, add=TRUE, zlim=c(min(cutoffs),max(cutoffs)), breaks=cutoffs, col=color.ramp, lab.breaks=cutoffs, legend.width=3, graphics.reset=TRUE, legend.args=list(text=label.text,cex=1.5,side=1,line=2.25))
-		# image.plot(legend.only=TRUE, add=TRUE, zlim=c(min(cutoffs),max(cutoffs)), col=as.character(colorRampPalette(color.ramp)(baseline.map@data$variable)), legend.width=3, graphics.reset=TRUE, legend.args=list(text=label.text,cex=1.5,side=1,line=2.25))
-		# image.plot(legend.only=TRUE, add=TRUE, zlim=c(min(cutoffs),max(cutoffs)), col=color.ramp, lab.breaks=cutoffs, legend.width=3, legend.args=list(text=label.text,cex=1.5,side=1,line=2.25)) # nlevel=length(color.ramp), graphics.reset=TRUE)
-	}
-}	
-
-create.figure <- function(workspace, species.folder, baseline.scenario, future.scenario, spatial.variable, baseline.time, time.window, historical.cutoffs, future.cutoffs)
-{
+	# ================================================================================================
 	# Spatial Data
 	political <- readShapePoly(fn='H:/SpatialData/SpatialData/bound0m_shp.tar/bound0m_shp/bound_p_wgs84_study_area',IDvar='BOUND_P_',proj4string=CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'),verbose=TRUE)
 	ocean <- readShapePoly(fn='H:/SpatialData/SpatialData/bound0m_shp.tar/bound0m_shp/bound_p_wgs84_ocean',IDvar='BOUND_P_',proj4string=CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'),verbose=TRUE)
-
-	historical.data <- data.prep(data.file=paste(workspace,species.folder,'/Results/',baseline.scenario,'/mean.',baseline.scenario,'.',spatial.variable,'.',baseline.time,'.csv',sep=''),type='abundance',var.name=spatial.variable)
-	if (spatial.variable=='huc') { huc <- readShapePoly(fn='H:/SpatialData/SpatialData/HUCs/all.hucs2.included',IDvar='CBW_CODE',proj4string=CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'),verbose=TRUE) }
-
+	study.area <- readShapePoly(fn='H:/SpatialData/SpatialData/HUCs/study_area2_buffered3',IDvar='Id',proj4string=CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'),verbose=TRUE)
+	
+	if (spatial.variable=='huc') { huc <- readShapePoly(fn='H:/SpatialData/SpatialData/HUCs/all_hucs2_included_areas_km2',IDvar='CBW_CODE',proj4string=CRS('+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'),verbose=TRUE) }
+	
+	# ==================================================================================================
+	# Plotting data
+	if (data.type=='abundance') { historical.data <- data.prep(data.file=paste(workspace,species.folder,'/Results/',baseline.scenario,'/mean.',baseline.scenario,'.',spatial.variable,'.',baseline.time,'.csv',sep=''),type='abundance',var.name=spatial.variable) }
+	if (data.type=='productivity') { historical.data <- data.prep(data.file=paste(workspace,species.folder,'/Results/',baseline.scenario,'/',baseline.scenario,'.report.productivity.',spatial.variable,'.',baseline.time,'.csv',sep=''),type='productivity',var.name=spatial.variable) }
+	print(dim(historical.data))
+	
 	fut.data <- list()
 	gcms <- c('ccsm3','cgcm3','giss-er','hadcm3','miroc')
 	for (i in 1:5)
 	{
-		fut.data[[i]] <- data.prep(data.file=paste(workspace,species.folder,'/Results/',future.scenario[1],gcms[i],future.scenario[2],'/abs.change.',future.scenario[1],gcms[i],future.scenario[2],'.',spatial.variable,'.',time.window[1],'.csv',sep=''),type='abundance',var.name=spatial.variable)
-		# print(head(fut.data[[i]]))
+		if (data.type=='abundance') { fut.data[[i]] <- data.prep(data.file=paste(workspace,species.folder,'/Results/',future.scenario[1],gcms[i],future.scenario[2],'/abs.change.',future.scenario[1],gcms[i],future.scenario[2],'.',spatial.variable,'.',time.window[1],'.csv',sep=''),type='abundance',var.name=spatial.variable) }
+		if (data.type=='productivity') { fut.data[[i]] <- data.prep(data.file=paste(workspace,species.folder,'/Results/',future.scenario[1],gcms[i],future.scenario[2],'/',future.scenario[1],gcms[i],future.scenario[2],'.report.productivity.',spatial.variable,'.',time.window[1],'.csv',sep=''),type='productivity',var.name=spatial.variable) }
+		print(dim(fut.data[[i]]))
 	}
 
+	# =====================================================================================================
+	# Plotting, making a figure
 	if (is.null(dev.list())) { dev.new() } # This may start giving an error for an invalid screen number.  Quit R and start over if this happens. 
-	png(paste(workspace,species.folder,'/analysis/',future.scenario[1],'abs.change.',time.window[1],future.scenario[2],'.png',sep=''), width=1200, height=800)
+	png(paste(workspace,species.folder,'/analysis/',future.scenario[1],data.type,'.',time.window[1],future.scenario[2],'.png',sep=''), width=1200, height=800)
 		
 		split.screen(
 			figs=matrix( # L, R, B, T; Ordered upper left corner to upper right, then bottom row.
@@ -104,7 +57,7 @@ create.figure <- function(workspace, species.folder, baseline.scenario, future.s
 			{
 				screen(n=i)
 				map.plot(
-					baseline.data=fut.data[[i]],
+					the.data=fut.data[[i]],
 					distribution.data=historical.data,
 					cutoffs=cutoffs,
 					model.name=paste(time.window[2],c('CCSM3','CGCM3.1','GISS-ER','MIROC3.2','UKMO-\nHadCM3'),sep='\n')[i], 
@@ -114,7 +67,9 @@ create.figure <- function(workspace, species.folder, baseline.scenario, future.s
 					color.ramp=brewer.pal((length(cutoffs)-1),name='PRGn'),
 					margins=c(3,3,0.5,0.5),
 					political=political,
-					ocean=ocean
+					ocean=ocean,
+					study.area=study.area,
+					data.type=data.type
 					)
 				close.screen(i)
 			}
@@ -122,7 +77,7 @@ create.figure <- function(workspace, species.folder, baseline.scenario, future.s
 			{
 				screen(i,new=TRUE)
 				map.plot(
-					baseline.data=fut.data[[i]], 
+					the.data=fut.data[[i]], 
 					distribution.data=historical.data,
 					cutoffs=cutoffs,
 					model.name=paste(time.window[2],c('CCSM3','CGCM3.1','GISS-ER','MIROC3.2','UKMO-\nHadCM3'),sep='\n')[i], 
@@ -131,9 +86,11 @@ create.figure <- function(workspace, species.folder, baseline.scenario, future.s
 					spatial.data=huc,
 					color.ramp=brewer.pal((length(cutoffs)-1),name='PRGn'),
 					margins=c(3,3,0.5,4.5),
-					label.text='  delta',
+					label.text='  births-\n  deaths', # '  delta',
 					political=political,
-					ocean=ocean
+					ocean=ocean,
+					study.area=study.area,
+					data.type=data.type
 					)
 				close.screen(i)
 			}
@@ -141,32 +98,37 @@ create.figure <- function(workspace, species.folder, baseline.scenario, future.s
 		cutoffs <- historical.cutoffs
 		screen(6,new=TRUE)
 		map.plot(
-			baseline.data=historical.data,
+			the.data=historical.data,
 			distribution.data=historical.data,
 			cutoffs=cutoffs,
-			model.name='BASELINE\nABUNDANCE', 
-			add.legend=TRUE,
+			model.name='BASELINE', # 'BASELINE\nABUNDANCE', 
+			add.legend=FALSE, # TRUE,
 			include.axes=TRUE,	
 			spatial.data=huc,
-			color.ramp=brewer.pal((length(cutoffs)-1),name='YlGn'),
+			color.ramp=brewer.pal((length(cutoffs)-1),name='PRGn'), # brewer.pal((length(cutoffs)-1),name='YlGn'),
 			margins=c(3,3,0.5,4.5),
 			label.text='  females',
 			political=political,
-			ocean=ocean
+			ocean=ocean,
+			study.area=study.area,
+			data.type=data.type
 			)
 		close.screen(6)
 	dev.off()
 
 }
 
+# =======================================================================================
+# Function Call
 create.figure(
 	workspace='H:/HexSim/Workspaces/',
 	species.folder='wolverine_v1',
 	baseline.scenario='gulo.023.baseline',
-	future.scenario=c('gulo.023.a2.','.swe'), # '.swe' '.biomes'
+	future.scenario=c('gulo.023.a2.',''), # '' '.swe' '.biomes'
+	data.type='productivity', # 'abundance'
 	spatial.variable='huc',
-	baseline.time='41.50',
-	time.window=c('41.50','MID-2100s'), # c('100.109','LATE-2100s'), #'100.109', #'41.50'
-	historical.cutoffs=c(0,5,10,25,50,75,100,150),
-	future.cutoffs=c(-125,-75,-30,-15,-5,0,5,15,30,75,125)
+	baseline.time='21.50',
+	time.window=c('81.109','LATE-2100s'), # c('100.109','LATE-2100s'), #'100.109', #'41.50'
+	historical.cutoffs=c(-1,-0.5,-0.25,-0.10,-0.05,0.05,0.10,0.25,0.5,1), # c(-75,-50,-30,-15,-5,5,15,30,50,75), # c(0,5,10,25,50,75,100,150),
+	future.cutoffs=c(-1,-0.5,-0.25,-0.10,-0.05,0.05,0.10,0.25,0.5,1) # c(-75,-50,-30,-15,-5,5,15,30,50,75) # c(-125,-75,-30,-15,-5,0,5,15,30,75,125)
 	)
