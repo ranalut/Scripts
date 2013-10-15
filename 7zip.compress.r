6f4473d463122723a1eb0369c086ac6803dded00
Chad Wilsey <cbwilsey@uw.edu>
2013-10-15 15:39:30 -0700

    7zip.compress.r works now!

--- 7zip.compress.r
+++ 7zip.compress.r
@@ -1,20 +1,19 @@
 
 # Compress log files to 7zip files.
 
-# This is not a successful R script.
-# I wasn't able to get the R script to work.
-# Copy 7z.exe to the results folder in which you plan to do compression.
-# open a cmd window and move the appropriate results directory
-# command:
-# 7z.exe a gulo.log.files.7z *.log -r -mmt
+source('scenario.vector.r')
 
-# command <- 'H:\\HexSim\\Workspaces\\wolverine_v1\\Results\\gulo.023.a2.ccsm3\\gulo.023.a2.ccsm3-[1]\\>f:\\pnwccva_data2\\7za920\\7za.exe a -t7z H:\\HexSim\\Workspaces\\wolverine_v1\\Results\\gulo.023.a2.ccsm3\\gulo.023.a2.ccsm3-[1]\\gulo.023.a2.ccsm3.log.7z *.log -mx7'
+scenarios <- scenarios.vector(
+					base.sim='lynx.050.', 
+					gcms=c('baseline','ccsm3','cgcm3','giss-er','hadcm3','miroc'), 
+					other=c('','.35')
+					)
 
-# command <- 'f:\\pnwccva_data2\\7za920\\7za.exe l H:\\HexSim\\Workspaces\\wolverine_v1\\Results\\gulo.023.a2.ccsm3\\gulo.023.a2.ccsm3-[1]\\gulo.023.a2.ccsm3.log.7z'
+for (i in scenarios)
+{				
+	command <- paste('I: && cd I:\\HexSim\\Workspaces\\lynx_v1\\Results\\ && 7z a logs.',i,'.7z -r ',i,'\\*.log -mx=5 -mmt=on -ms=off',sep='')
+	# print(command); stop('cbw')
 
-# command <- 'H: && cd H:\\HexSim\\Workspaces\\wolverine_v1\\Results\\gulo.017.a2.ccsm3\\ && 7z.exe a gulo.017.ccms3.log.files.7z *.log -r -mmt'
-
-# shell(command)
-# shell(shell='%windir%\\SysWoW64\\cmd.exe', command)
-
-
+	shell(command)
+	# stop('cbw')
+}
