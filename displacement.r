c24c3639869f9eb6afb8ab7d547aaa1d69d29de7
Chad Wilsey <cbwilsey@uw.edu>
2013-10-21 16:20:51 -0700

    displacement.r look at squirrels

--- displacement.r
+++ displacement.r
@@ -2,7 +2,10 @@
 workspace <- 'f:/pnwccva_data2/hexsim/workspaces/spotted_frog_v2'
 base.sim <- 'rana.lut.103e2'
 
-displace <- read.csv(paste(workspace,'/results/',base.sim,'/',base.sim,'-[1]/',base.sim,'_REPORT_displacement_rana_lut_0_10.csv',sep=''),header=TRUE)
+workspace <- 'd:/data/wilsey/hexsim/workspaces/town_squirrel_v1'
+base.sim <- 'squirrel.006c.100'
+
+displace <- read.csv(paste(workspace,'/results/',base.sim,'/',base.sim,'-[1]/',base.sim,'_REPORT_displacement_squirrels_0_5.csv',sep=''),header=TRUE)
 displace$Meters.Displaced <- round(displace$Meters.Displaced)
 print(table(displace$Meters.Displaced))
 hist(displace$Meters.Displaced)
