fbb309cff9a21d8b406b5d5291386a33082ca8cd
Chad Wilsey <cbwilsey@uw.edu>
2014-03-21 21:46:56 -0700

    update veg.file.paths.r

--- veg.file.paths.r
+++ veg.file.paths.r
@@ -9,7 +9,7 @@ build.paths <- function(theGCM, var.index=1)
   file.name.cru <- 'wna30sec_cru_ts_2.10_biome_30-year_mean_' # c('deficit_mam_','wna30sec_cru_ts2.1_lpj_afirefrac_','wna30sec_cru_ts_2.10_mtco_','wna30sec_cru_ts_2.10_mtwa_')
 
   names1 <- paste('L:/space_lawler/shared/pnwccva-vegetationdata/26jul13_outputs/biome_modal_30yr_cru_ts_2.10/',file.name.cru[z],seq(1961,2000,1),'.nc',sep='')
-  names2 <- paste('L:/space_lawler/shared/pnwccva-vegetationdata/biome_model_30yr_a2/',file.name.a2.pre[z],theGCM,file.name.a2.suf[z],seq(2001,2099,1),'.nc',sep='')
+  names2 <- paste('L:/space_lawler/shared/pnwccva-vegetationdata/26jul13_outputs/biome_modal_30yr_a2/',file.name.a2.pre[z],theGCM,file.name.a2.suf[z],seq(2001,2099,1),'.nc',sep='')
   the.names <- c(names1,names2)
   return(the.names)
 }
