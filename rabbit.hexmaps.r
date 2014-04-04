94e4370d621332b85a14dd92c147ab079d6384a6
Chad Wilsey <cbwilsey@uw.edu>
2014-04-04 12:45:09 -0700

    update rabbit.hexmaps.r

--- rabbit.hexmaps.r
+++ rabbit.hexmaps.r
@@ -25,9 +25,12 @@ spp.folder <- 'rabbit_v1'
 
 run.hex.grid.distn <- 	'n'
 run.distn <- 			'n'
-run.hex.grid <-			'y'
+run.hex.grid.lulc <- 	'y'
+run.lulc <- 			'y'
+run.hist.lulc <- 		'n'
+run.hex.grid <-			'n'
 run.move.ave <-			'n'
-run.biomes <- 			'y'
+run.biomes <- 			'n'
 run.hist.biomes <- 		'n'
 run.initial <- 			'n' # Not updated for squirrel
 run.fire <- 			'n' # May need to add this
@@ -72,6 +75,88 @@ if (run.distn=='y')
 		dimensions=c(1750,1859)
 		)
 }
+
+# =======================================================================================
+# Load LULC hex.grid
+if (run.hex.grid.lulc=='y')
+{
+	hex.grid <- load.hex.grid(
+			centroid.file="L:/Space_Lawler/Shared/Wilsey/Postdoc/HexSim/Workspaces/sage_grouse_v2/Spatial Data/albers_centroids_1km_lulc.dbf", # "F:/PNWCCVA_Data2/HexSim/Workspaces/sage_grouse_v2/Spatial Data/albers_centroids_1km_lulc.dbf",   
+			file.format='dbf',
+			proj4='+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
+			)
+	hex.grid[[2]] <- SpatialPoints(hex.grid[[2]], proj4string=CRS('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0'))
+}
+
+# ========================================================================================
+# LULC projections
+if (run.lulc=='y')
+{
+	source('lulc.file.paths.r')
+	source('time.series.2.hexmaps.r')
+	theSRESs <- c('a2','a1b')
+	
+	for (i in 2) # 1:2
+	{
+		startTime <- Sys.time()
+		the.names <- build.paths(theSRES=theSRESs[i])
+		cat('built names\n')
+
+		hexmap.time.series(
+			the.names=the.names, 
+			output.wksp=output.wksp, spp.folder=spp.folder, 
+			theGCM=theSRESs[i], 
+			hexmap.base.name='lulc', 
+			hex.grid=hex.grid,
+			changeTable=data.frame(matrix(c(seq(1,255,1),c(18,2,18,18,18,6,18,18,18,18,18,18,13,14,18,18,18),rep(18,238)),ncol=2)),
+			variable=NA,
+			start.yr=32,
+			end.yr=NA,
+			buffer=NA, ag.fact=4, fun=modal, crop=TRUE,
+			ver=i
+			)
+	}
+}
+
+# Just historical LULC
+if (run.hist.lulc=='y')
+{
+	source('lulc.file.paths.r')
+	source('time.series.2.hexmaps.r')
+	theSRESs <- c('a2','a1b')
+	
+	for (i in 1) # 1:2
+	{
+		startTime <- Sys.time()
+		the.names <- build.paths(theSRES=theSRESs[i])
+		cat('built names\n')
+
+		hexmap.time.series(
+			the.names=the.names, 
+			output.wksp=output.wksp, spp.folder=spp.folder, 
+			theGCM=theSRESs[i], 
+			hexmap.base.name='lulc.hist', 
+			hex.grid=hex.grid,
+			changeTable=data.frame(matrix(c(seq(1,255,1),c(18,2,18,18,18,6,18,18,18,18,18,18,13,14,18,18,18),rep(18,238)),ncol=2)),
+			variable=NA,
+			start.yr=32,
+			end.yr=40,
+			buffer=NA, ag.fact=4, fun=modal, crop=TRUE
+			)
+	}
+}
+
+	# test<-nc.2.hxn(
+			# variable='lulc', 
+			# nc.file='h:/LULC/conus2099_r2b.tif', 
+			# hex.grid=hex.grid[[2]], 
+			# theCentroids=hex.grid[[1]],
+			# max.value=Inf,
+			# # changeTable=data.frame(matrix(c(seq(1,12,1),c(0,0,0,0,0,0,0,2,1,0,0,0)),ncol=2)), # grasslands steppe (8) and shrub steppe (9)
+			# hexsim.wksp=hexsim.wksp, hexsim.wksp2=hexsim.wksp2, output.wksp=output.wksp, output.wksp2=output.wksp2, spp.folder=spp.folder, hexmap.name='lulc.2099',
+			# dimensions=c(1750,1859)
+			# )
+# }
 
 # ==========================================================================================
 # Load WGS 84 hex.grid for extracting spatial data
