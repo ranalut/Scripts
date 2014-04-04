a10cfd9cd11fddfb32271412ecf41ffc4bbc9114
Chad Wilsey <cbwilsey@uw.edu>
2014-04-04 16:54:33 -0700

    update build.sdm.r

--- build.sdm.r
+++ build.sdm.r
@@ -4,26 +4,29 @@ library(randomForest)
 source('export.hexmaps.r')
 
 # Extract the hexmap data
-hexsim.wksp <- 'D:/data/wilsey/HexSim/' # 'H:/HexSim/' # 'F:/PNWCCVA_Data2/HexSim/' # 'D:/data/wilsey/HexSim/'
-hexsim.wksp2 <- 'D:\\data\\wilsey\\hexsim' # 'H:\\HexSim' # 'F:\\PNWCCVA_Data2\\HexSim' # 'D:\\data\\wilsey\\hexsim'
-output.wksp <- 'L:/Space_Lawler/Shared/Wilsey/PostDoc/HexSim/' # 'H:/HexSim/' # 'E:/HexSim/' # 'D:/data/wilsey/HexSim/' # 'F:/PNWCCVA_Data2/HexSim/' 
-output.wksp2 <- 'L:\\Space_Lawler\\Shared\\Wilsey\\PostDoc\\HexSim' # 'H:\\HexSim' # 'E:\\HexSim' # 'D:\\data\\wilsey\\hexsim' # 'F:\\PNWCCVA_Data2\\HexSim'
+hexsim.wksp <- 'L:/Space_Lawler/Shared/Wilsey/PostDoc/HexSim/' # 'D:/data/wilsey/HexSim/' # 'H:/HexSim/' # 'F:/PNWCCVA_Data2/HexSim/' # 'D:/data/wilsey/HexSim/'
+hexsim.wksp2 <- 'L:\\Space_Lawler\\Shared\\Wilsey\\Postdoc\\HexSim' # 'D:\\data\\wilsey\\hexsim' # 'H:\\HexSim' # 'F:\\PNWCCVA_Data2\\HexSim' # 'D:\\data\\wilsey\\hexsim'
 spp.folder <- 'rabbit_v1'
 
-export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name='ave.def.mam.ccsm3')
-export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name='ave.fire.ccsm3')
-export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name='ave.mtco.ccsm3')
-export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name='ave.mtwa.ccsm3')
-export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name='biomes.ccsm3')
-export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name='lulc.ccsm3')
-export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name='pyra2')
+map.names <- c('pyra2','all.water','ave.def.mam.ccsm3','ave.fire.ccsm3','ave.mtco.ccsm3','ave.mtwa.ccsm3','ccsm3.hist.biome','lulc.hist.a2')
+time.steps <- c(rep(1,6),30,32)
 
-
-
-
-
-
+for (i in 1:length(map.names))
+{
+	export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name=map.names[i],time.step=time.steps[i]) # stop('cbw')
+}
 
 # Build random forest model
+
+the.data <- read.csv(paste(hexsim.wksp,'workspaces/',spp.folder,'/Analysis/',map.names[1],'.csv',sep=''))
+for (i in 2:length(map.names))
+{
+	temp <- read.csv(paste(hexsim.wksp,spp.folder,'/Analysis/',map.names[i],'.csv',sep=''),row.names=1)
+	the.data <- cbind(the.data,temp)
+}
+colnames(the.data) <- c('hexid','pyra2','water','def.mam','fire','mtco','mtwa','biomes','lulc')
+the.data <- the.data[the.data$water!=1 & the.data$def.mam!=0,]
+
+# all.pts <- sample(the.data$hexid, size=10000)
 
 
