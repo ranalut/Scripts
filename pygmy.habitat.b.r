4fdab29b5566bbced3112f12f36aea8321f23c56
Chad Wilsey <cbwilsey@uw.edu>
2014-04-21 11:40:08 -0700

    update pygmy.habitat.r

--- pygmy.habitat.b.r
+++ pygmy.habitat.b.r
@@ -13,8 +13,8 @@ spp.folder <- 'rabbit_v1'
 theGCMs <- c('CCSM3','CGCM3.1_t47','GISS-ER','MIROC3.2_medres','UKMO-HadCM3')
 
 # index <- 1
-markers <- rep('',7) # rep('',7) # c(index,rep('',5),index) # rep('-b',7) 
-scenario <- 'clim.' # 'clim.' # '' # 'lulc.' # 'veg.'
+markers <- rep('-b',7) # rep('',7) # c(index,rep('',5),index) # rep('-b',7) 
+scenario <- 'veg.' # 'clim.' # '' # 'lulc.' # 'veg.'
 
 theGCM <- theGCMs[index]
 map.names <- c('all.water',paste(c('ave.def.mam.','ave.fire.','ave.mtco.','ave.mtwa.'),theGCM,sep=''),paste(theGCM,'.biomes.a2',sep=''),'lulc.a2') # 'pyra2',
@@ -29,23 +29,35 @@ for (n in c(seq(1,110,1),1))
 	# print(time.steps)
 	if (time.steps['lulc'] < 32) { time.steps['lulc'] <- 32 }
 	time.steps['water'] <- 1 # Water only has one hexmap.
-	if (scenario=='clim.') { time.steps['biomes'] <- 30; time.steps['lulc'] <- 32 } # Only climate changes.
-	if (scenario=='lulc.') { time.steps['biomes'] <- 30; time.steps[c('def.mam','fire','mtco','mtwa')] <- 1 } # Only lulc changes.
-	if (scenario=='veg.') { time.steps['lulc'] <- 32; time.steps[c('def.mam','fire','mtco','mtwa')] <- 1 } # Only veg changes.
+	if (scenario=='') { export.maps <- c(2:7) }
+  if (scenario=='clim.') { time.steps['biomes'] <- 30; time.steps['lulc'] <- 32; export.maps <- c(2:5) } # Only climate changes.
+	if (scenario=='lulc.') { time.steps['biomes'] <- 30; time.steps[c('def.mam','fire','mtco','mtwa')] <- 1; export.maps <- 7 } # Only lulc changes.
+	if (scenario=='veg.') { time.steps['lulc'] <- 32; time.steps[c('def.mam','fire','mtco','mtwa')] <- 1; export.maps <- 6 } # Only veg changes.
 	print(time.steps)# ; stop('cbw')
-	
-	for (i in 1:length(map.names))
+	if (n==1)
 	{
-		export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name=map.names[i],time.step=time.steps[i],marker=markers[i]) # stop('cbw')
+  	for (i in 1:length(map.names))
+  	{
+  		export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name=map.names[i],time.step=time.steps[i],marker=markers[i]) # stop('cbw')
+  	}
+	}
+	else
+	{
+	   for (i in export.maps)
+  	{
+  		export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, spp.folder=spp.folder, hexmap.name=map.names[i],time.step=time.steps[i],marker=markers[i]) # stop('cbw')
+  	}
 	}
 	cat('exported maps...')
 	
 	# Assemble data
 	the.data <- read.csv(paste(hexsim.wksp,'workspaces/',spp.folder,'/Analysis/',map.names[1],markers[1],'.csv',sep=''))
-	for (i in 2:length(map.names))
+	cat(col.names[2],' ')
+  for (i in 2:length(map.names))
 	{
 		temp <- read.csv(paste(hexsim.wksp,'workspaces/',spp.folder,'/Analysis/',map.names[i],markers[i],'.csv',sep=''),row.names=1)
 		the.data <- cbind(the.data,temp)
+		cat(col.names[i+1],' ')
 	}
 	
 	colnames(the.data) <- col.names
@@ -54,7 +66,7 @@ for (n in c(seq(1,110,1),1))
 	the.data$obs <- the.data$pyra2
 	the.data$biomes <- factor(the.data$biomes,levels=seq(0,11,1))
 	the.data$lulc <- factor(the.data$lulc, levels=c(0,2,6,13,14,18))
-	cat('assembled data...')
+	cat('assembled data\n')
 	
 	# Load Model
 	load(file='l:/space_lawler/shared/wilsey/postdoc/hexsim/workspaces/rabbit_v1/analysis/rf.model.v1.rdata')
@@ -62,7 +74,7 @@ for (n in c(seq(1,110,1),1))
 	pred.spp.distn <- predict(rf.model, newdata=the.data, type='prob',progress='window')
 
 	write.csv(data.frame(hexid=the.data$hexid,Pred=pred.spp.distn[,2]),paste('l:/space_lawler/shared/wilsey/postdoc/hexsim/workspaces/rabbit_v1/analysis/rf.model.pred',markers[1],'.csv',sep=''),row.names=FALSE)
-	file.remove(paste(hexsim.wksp,'workspaces/',spp.folder,'/Analysis/',map.names,markers,'.csv',sep=''))
+	file.remove(paste(hexsim.wksp,'workspaces/',spp.folder,'/Analysis/',map.names[export.maps],markers[export.maps],'.csv',sep=''))
 	cat('completed prediction\n')
 	
 	hexmap.name <- paste('hab.',scenario,theGCM,sep='')
@@ -72,7 +84,7 @@ for (n in c(seq(1,110,1),1))
 	dir.create(paste(hexsim.wksp,'Workspaces/',spp.folder,'/Spatial Data/Hexagons/',hexmap.name,'/',sep=''))
 	file.copy(
 		from=paste(hexsim.wksp,'Workspaces/',spp.folder,'/Spatial Data/Hexagons/temp.',hexmap.name,'/temp.',hexmap.name,'.1.hxn',sep=''),
-		to=paste(hexsim.wksp,'Workspaces/',spp.folder,'/Spatial Data/Hexagons/',hexmap.name,'/',hexmap.name,'.',n,'.hxn',sep='')
+		to=paste(hexsim.wksp,'Workspaces/',spp.folder,'/Spatial Data/Hexagons/',hexmap.name,'/',hexmap.name,'.',n,'.hxn',sep=''),overwrite=TRUE
 		)
 	cat('hexmap imported\n')
 	
