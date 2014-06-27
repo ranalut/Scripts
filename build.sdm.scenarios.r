1477a84d36cb497299397d61c94f4aa8bcf9e4ef
Chad Wilsey <cbwilsey@uw.edu>
2014-06-27 10:29:40 -0700

    update build.sdm.scenarios.r

--- build.sdm.scenarios.r
+++ build.sdm.scenarios.r
@@ -1,21 +1,24 @@
 
 theGCMs <- c('CCSM3','CGCM3.1_t47','GISS-ER','MIROC3.2_medres','UKMO-HadCM3')
-theScenarios <- c('','clim.','lulc.','veg.')
-base.name <- 'rabbit.020.'
-workspace <- '//cfr.washington.edu/main/Space/Lawler/Shared/Wilsey/PostDoc/Hexsim/Workspaces/rabbit_v1/Scenarios/'
-base.scenario <- paste(workspace,'rabbit.020.all.ccsm3.xml',sep='')
+theScenarios <- c('','clim.','veg.')
+base.name <- 'krat.05.' # 'rabbit.020.'
+thresholds <- as.character(c(0.55,0.495,0.772)) # as.character(c(0.63,0.55,0.656))
+workspace <- 'krat_v1' # 'rabbit_v1'
+workspace <- paste('//cfr.washington.edu/main/Space/Lawler/Shared/Wilsey/PostDoc/Hexsim/Workspaces/',workspace,'/Scenarios/', sep='')
+
+base.scenario <- paste(workspace,base.name,'all.ccsm3.xml',sep='')
 
 base <- readLines(base.scenario)
-print(base[c(61,93,162,209)])
+print(base[c(45,61,93,162,209)])
 
 for (i in theGCMs)
 {
-	for (j in theScenarios)
+	for (j in 1:3)
 	{
-		temp <- gsub(pattern='hab.ccsm3',replacement=paste('hab.',j,i,sep=''),x=base, ignore.case=TRUE)
-		print(temp[c(61,93,162,209)])# ; stop('cbw')
-		writeLines(temp, paste(workspace,'rabbit.020.hab.',j,i,'.xml',sep=''))
+		temp <- gsub(pattern='hab.ccsm3',replacement=paste('hab.v2.',theScenarios[j],i,sep=''),x=base, ignore.case=TRUE)
+		temp <- gsub(pattern='0.625',replacement=thresholds[j],x=temp, ignore.case=TRUE)
+		print(temp[c(45,61,93,162,209)])# ; stop('cbw')
+		writeLines(temp, paste(workspace,base.name,'hab.v2.',theScenarios[j],i,'.xml',sep=''))
 		# stop('cbw')
 	}
 }
-
