5e9b4c768b3d96ab54ac596ddafee1c3f21e8e24
Chad Wilsey <cbwilsey@uw.edu>
2014-10-01 15:27:09 -0700

    update consolidate.maps.r

--- consolidate.maps.r
+++ consolidate.maps.r
@@ -1,39 +1,54 @@
 
-
-source('export.hexmaps.r')
-
-
 # Extract the hexmap data
 hexsim.wksp <- 'D:/data/wilsey/HexSim/' # 'L:/Space_Lawler/Shared/Wilsey/PostDoc/HexSim/' # 'D:/data/wilsey/HexSim/' # 'H:/HexSim/' # 'F:/PNWCCVA_Data2/HexSim/' # 'D:/data/wilsey/HexSim/'
 hexsim.wksp2 <- 'D:\\data\\wilsey\\hexsim' # 'L:\\Space_Lawler\\Shared\\Wilsey\\Postdoc\\HexSim' # 'D:\\data\\wilsey\\hexsim' # 'H:\\HexSim' # 'F:\\PNWCCVA_Data2\\HexSim' # 'D:\\data\\wilsey\\hexsim'
 output.wksp <- 'L:/Space_Lawler/Shared/Wilsey/PostDoc/HexSim/' # '//cfr.washington.edu/main/Space/Lawler/Shared/Wilsey/PostDoc/HexSim/'
 output.wksp2 <- 'L:\\Space_Lawler\\Shared\\Wilsey\\Postdoc\\HexSim' # '\\\\cfr.washington.edu\\main\\Space\\Lawler\\Shared\\Wilsey\\PostDoc\\HexSim'
 
-spp.folder <- 'rabbit_v1' # 'woodpecker_v1' # 'nutcracker_v1' 
-map.name <- 'distribution'
-scenario <- 'rabbit.020.hab.v2.'
-theGCMs <- c('ccsm3','cgcm3','giss-er','miroc','hadcm3')
-model.types <- list(c('','.clim','.veg'),c('full','clim','veg'))
-reps <- 25
-# parameters <- list(list(1,'eco','ECO_ID_U',c(17026:17097)), list(2,'huc','PNWCCVA_ID',c(1:1549)), list(3,'pa','OBJECTID', c(1:1252)))
+base.map <- read.csv(paste(output.wksp,'Workspaces/',spp.folder,'/Analysis/',parameters[[map]][2],'.csv',sep=''))
+base.map[1,2] <- 0
+base.map$Step_1 <= round(base.map$Step_1)
 
-time.steps <- seq(10,110,10)
-
-for (i in 1:reps)
+for (p in 1:3) # Loop through 3 model types.
 {
-	for (j in 1:length(time.steps))
+	for (k in theGCMs)
 	{
-		# export.hexmaps.spatial(hexsim.wksp2=hexsim.wksp2, output.wksp2=output.wksp2, spp.folder=spp.folder, hexmap.name=map.name,time.step=time.steps[i]) # stop('cbw')
+		full.scenario <- paste(scenario, model.types[[1]][p],k,sep='')
+		print(full.scenario)
+		
+		for (i in 1:reps)
+		{
+			for (j in 1:length(time.steps))
+			{
+				export.hexmaps.temp(hexsim.wksp2=hexsim.wksp2,output.wksp2=output.wksp2,spp.folder=spp.folder,scenario=full.scenario,n=i,time.step=time.steps[j],hexmap.name=map.name)
 
-		export.hexmaps <- function(hexsim.wksp2=hexsim.wksp2,spp.folder=spp.folder,scenario=scenario,n=i,hexmap.name=map.name)
-
-		
-		
-		
-		
+				temp <- read.csv(paste(output.wksp,'Workspaces/',spp.folder,'/Analysis/temp.csv',sep=''),header=TRUE)
+				
+				temp <- data.frame(base.map[,2],temp[,2])
+				colnames(temp) <- c('space','year')
+				# print(head(temp))
+				
+				temp <- aggregate(. ~ space, data=temp, FUN=sum)
+				temp[,2] <- round(temp[,2])
+				# print(head(temp))
+				
+				v.temp <- as.vector(temp[,2])
+				v.temp <- c(model.types[[2]][p],k,i,time.steps[j],v.temp)
+				names(v.temp) <- c('model','gcm','rep','year',temp[,1])
+				
+				if (k==theGCMs[1] & i==1 & j==1)
+				{
+					write.csv(t(v.temp),paste(output.wksp,'Workspaces/',spp.folder,'/Analysis/',scenario, model.types[[1]][p],parameters[[map]][2],'.csv',sep=''),quote=FALSE)
+				}
+				else
+				{
+					write.table(t(v.temp),paste(output.wksp,'Workspaces/',spp.folder,'/Analysis/',scenario, model.types[[1]][p],parameters[[map]][2],'.csv',sep=''), append=TRUE,quote=FALSE,sep=',',col.names=FALSE)
+				}
+				# stop('cbw')	
+			}
+		}
 	}
 }
-
 
 
 
