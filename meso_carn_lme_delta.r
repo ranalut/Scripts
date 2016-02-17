e283602718ab3451f70540242a8430646fe98b80
ranalut <chadwilsey@gmail.com>
2016-02-16 23:01:32 -0800

    update lme of deltas

--- meso_carn_lme_delta.r
+++ meso_carn_lme_delta.r
@@ -25,23 +25,28 @@ output2$gcm <- factor(output2$gcm)
 
 ly <- output2[output2$species=='lynx',]
 ly$d2050s_dens <- 100 * ly$d2050s/ly$AREA_SQKM
+ly$d2090s_dens <- 100 * ly$d2090s/ly$AREA_SQKM
 ly <- ly[100*ly$Y2000s/ly$AREA_SQKM > as.numeric(dens_thresh['lynx']),]
 
 hist(ly$d2050s_dens)
 # shapiro.test(ly$d2050s_dens) # significantly different from normal
+hist(ly$d2090s_dens)
 # stop('cbw')
 
 # m_ly1 <- lmer(d2050s ~ sens + gcm + (1 | ECO_CODE), data=ly, REML=FALSE) 
 # m_ly1_n <- lmer(d2050s ~ gcm + (1 | ECO_CODE), data=ly, REML=FALSE) 
-m_ly2 <- lmer(d2050s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=ly, REML=FALSE)
-m_ly2_n <- lmer(d2050s_dens ~ gcm + (1 + sens|ECO_CODE), data=ly, REML=FALSE)
+# m_ly2 <- lmer(d2050s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=ly, REML=FALSE)
+# m_ly2_n <- lmer(d2050s_dens ~ gcm + (1 + sens|ECO_CODE), data=ly, REML=FALSE)
+m_ly2 <- lmer(d2090s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=ly, REML=FALSE)
+m_ly2_n <- lmer(d2090s_dens ~ gcm + (1 + sens|ECO_CODE), data=ly, REML=FALSE)
 
 plot(m_ly2)
 qqnorm(resid(m_ly2), main="Q-Q plot for residuals")
 qqline(resid(m_ly2),col='red')
-stop('cbw')
-write.csv(as.data.frame(summary(m_ly2)[10]),"D:/Box Sync/PNWCCVA/2015 Outputs/lynx_lmer_coef.csv")
-sink("D:/Box Sync/PNWCCVA/2015 Outputs/lynx_lmer.txt")
+# stop('cbw')
+
+write.csv(as.data.frame(summary(m_ly2)[10]),"D:/Box Sync/PNWCCVA/MS_MesoCarnivores/Results/lynx_lmer_coef_2090s.csv")
+sink("D:/Box Sync/PNWCCVA/MS_MesoCarnivores/Results/lynx_lmer_2090s.txt")
 # anova(m_ly1_n,m_ly1) # Likelihood ratio tests. Both models are significant.
 cat('\n')
 print(summary(m_ly2)); cat('\n')
@@ -50,28 +55,33 @@ print(coefficients(m_ly2)); cat('\n')
 print(r.squaredLR(m_ly2,null=m_ly2_n)); cat('\n')
 print(r.squaredGLMM(m_ly2))
 sink()
-
+# stop('cbw')
 # =========================================
 # Wolverine 
 # =========================================
 
 w <- output2[output2$species=='wolverine',]
 w$d2050s_dens <- 100 * w$d2050s/w$AREA_SQKM
+w$d2090s_dens <- 100 * w$d2090s/w$AREA_SQKM
 w <- w[100*w$Y2000s/w$AREA_SQKM > as.numeric(dens_thresh['wolverine']),]
 
 hist(w$d2050s_dens)
 # shapiro.test(w$d2050s_dens) # significantly different from normal, doesn't matter
-stop('cbw')
+hist(w$d2090s_dens)
+# stop('cbw')
 
 # m_w1 <- lmer(d2050s ~ sens + gcm + (1 | ECO_CODE), data=w, REML=FALSE) 
 # m_w1_n <- lmer(d2050s ~ gcm + (1 | ECO_CODE), data=w, REML=FALSE) 
-m_w2 <- lmer(d2050s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=w, REML=FALSE)
-m_w2_n <- lmer(d2050s_dens ~ gcm + (1 + sens|ECO_CODE), data=w, REML=FALSE)
+# m_w2 <- lmer(d2050s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=w, REML=FALSE)
+# m_w2_n <- lmer(d2050s_dens ~ gcm + (1 + sens|ECO_CODE), data=w, REML=FALSE)
+m_w2 <- lmer(d2090s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=w, REML=FALSE)
+m_w2_n <- lmer(d2090s_dens ~ gcm + (1 + sens|ECO_CODE), data=w, REML=FALSE)
 
 plot(m_w2)
 qqnorm(resid(m_w2), main="Q-Q plot for residuals")
-write.csv(as.data.frame(summary(m_w2)[10]),"D:/Box Sync/PNWCCVA/2015 Outputs/wolverine_lmer_coef.csv")
-sink("D:/Box Sync/PNWCCVA/2015 Outputs/wolverine_lmer.txt")
+qqline(resid(m_w2),col='red')
+write.csv(as.data.frame(summary(m_w2)[10]),"D:/Box Sync/PNWCCVA/MS_MesoCarnivores/Results/wolverine_lmer_coef_2090s.csv")
+sink("D:/Box Sync/PNWCCVA/MS_MesoCarnivores/Results/wolverine_lmer_2090s.txt")
 # anova(m_w1_n,m_w1) # Likelihood ratio tests. Both models are significant.
 print(summary(m_w2))
 print(anova(m_w2_n,m_w2))
@@ -79,6 +89,7 @@ print(coefficients(m_w2))
 print(r.squaredLR(m_w2,null=m_w2_n)); cat('\n')
 print(r.squaredGLMM(m_w2))
 sink()
+# stop('cbw')
 
 # ============================================
 # Fisher
@@ -86,19 +97,26 @@ sink()
 
 f <- output2[output2$species=='fisher',]
 f$d2050s_dens <- 100 * f$d2050s/f$AREA_SQKM
+f$d2090s_dens <- 100 * f$d2090s/f$AREA_SQKM
 f <- f[100*f$Y2000s/f$AREA_SQKM > as.numeric(dens_thresh['fisher']),]
+
 hist(f$d2050s_dens)
 # shapiro.test(f$d2050s_dens) # significantly different from normal
+hist(f$d2090s_dens)
 
 # m_f1 <- lmer(d2050s ~ sens + gcm + (1 | ECO_CODE), data=f, REML=FALSE) 
 # m_f1_n <- lmer(d2050s ~ gcm + (1 | ECO_CODE), data=f, REML=FALSE) 
-m_f2 <- lmer(d2050s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=f, REML=FALSE)
-m_f2_n <- lmer(d2050s_dens ~ gcm + (1 + sens|ECO_CODE), data=f, REML=FALSE)
+# m_f2 <- lmer(d2050s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=f, REML=FALSE)
+# m_f2_n <- lmer(d2050s_dens ~ gcm + (1 + sens|ECO_CODE), data=f, REML=FALSE)
+m_f2 <- lmer(d2090s_dens ~ sens + gcm + (1 + sens|ECO_CODE), data=f, REML=FALSE)
+m_f2_n <- lmer(d2090s_dens ~ gcm + (1 + sens|ECO_CODE), data=f, REML=FALSE)
 
 plot(m_f2)
 qqnorm(resid(m_f2), main="Q-Q plot for residuals")
-write.csv(as.data.frame(summary(m_f2)[10]),"D:/Box Sync/PNWCCVA/2015 Outputs/fisher_lmer_coef.csv")
-sink("D:/Box Sync/PNWCCVA/2015 Outputs/fisher_lmer.txt")
+qqline(resid(m_f2),col='red')
+
+write.csv(as.data.frame(summary(m_f2)[10]),"D:/Box Sync/PNWCCVA/MS_MesoCarnivores/Results/fisher_lmer_coef_2090s.csv")
+sink("D:/Box Sync/PNWCCVA/MS_MesoCarnivores/Results/fisher_lmer_2090s.txt")
 # anova(m_f1_n,m_f1) # Likelihood ratio tests. Both models are significant.
 print(summary(m_f2))
 print(anova(m_f2_n,m_f2))
